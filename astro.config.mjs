8683dc0c265f829e903d6695eeb8c0534b4b2449
Zeida <zeidaromen97@gmail.com>
2024-09-20 12:34:44 +0100

    config github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://zeida.github.io',
+	base: 'MyPortfolio',
 	integrations: [mdx(), sitemap()],
 });
