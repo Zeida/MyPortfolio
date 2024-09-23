486ce10582594c9a0d3510cafa0f27d4260cd61e
Zeida <zeidaromen97@gmail.com>
2024-09-23 14:53:08 +0100

    page split in two change light/dark mode toggle button added social links and icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import svelte from '@astrojs/svelte';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://zeida.github.io',
-	base: '/MyPortfolio',
-	integrations: [mdx(), sitemap()],
+    site: 'https://zeida.github.io',
+    base: '/MyPortfolio',
+    integrations: [mdx(), sitemap(), svelte()],
 });
